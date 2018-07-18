8fced731ccca1b55b66f3c313e95cc63b28f5570
Nabil Droussi <nabil.droussi@aurea.com>
2018-07-18 08:16:59 +0000

    NewTask and Worker implementation in which I learned:  1. Round-Robin dispatching.  2. Durable queue and message so when RabbitMQ crashes message are re-delivered after recovery.

---
+++ WorkQueues/WorkQueues.NewTask/NewTask.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Publisher
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" [*] Program started, enter empty line to exit.");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "durable_task_queue_new",
+                                        durable: true,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    channel.BasicQos(0, 1, false);
+
+                    String message;
+                    Console.WriteLine("Write your first message: ");
+                    while (!String.IsNullOrWhiteSpace(message = Console.ReadLine()))
+                    {
+                        var body = Encoding.UTF8.GetBytes(message);
+                        var properties = channel.CreateBasicProperties();
+
+                        properties.Persistent = true;
+
+                        channel.BasicPublish(exchange: "",
+                                            routingKey: "durable_task_queue_new",
+                                            basicProperties: properties,
+                                            body: body);
+
+                        Console.WriteLine(" [x] Sent {0}, Enter a new one or enter empty message to exit.", message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WorkQueues/WorkQueues.Worker/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace HelloWorld.Consumer
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "durable_task_queue_new",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, ea) => {
+                        new Task(() => {
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body);
+
+                            Console.WriteLine(" [x] message received: {0}", message);
+
+                            var dotsCount = message.ToCharArray().Count(c => c == '.') - 1;
+
+                            Thread.Sleep(dotsCount * 1000);
+
+                            Console.WriteLine(" [x] Done");
+
+                            channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                                            multiple: false);
+                        }).Start();
+                    };
+
+                    channel.BasicConsume(queue: "durable_task_queue_new",
+                                        autoAck: false,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}

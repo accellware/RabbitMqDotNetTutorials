7f206094a6e194e2f6977d2309befbcee22f8e6a
Nabil Droussi <nabil.droussi@aurea.com>
2018-07-18 07:01:05 +0000

    Hello World to RabbitMQ.

---
+++ HelloWorld/HelloWorld.Consumer/Receive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace HelloWorld.Consumer
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, ea) => {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(" [x] message received: \n{0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.Publisher/Send.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace HelloWorld.Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Program started.");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    while(true)
+                    {
+                        string input = Console.ReadLine();
+                        string message = $"{input}\nDate: {DateTime.UtcNow}";
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                            routingKey: "hello",
+                                            basicProperties: null,
+                                            body: body);
+
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadKey();
+        }
+    }
+}

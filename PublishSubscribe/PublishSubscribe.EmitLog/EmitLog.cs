3102c0398f05f251616b5197a63ea067dfc768b2
Nabil Droussi <nabil.droussi@aurea.com>
2018-07-19 18:07:13 +0000

    Implemented Publish/Subscribe pattern.

---
+++ PublishSubscribe/PublishSubscribe.EmitLog/EmitLog.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Publisher
+{
+    class EmitLog
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" [*] Program started, enter empty line to exit.");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+                    
+                    String message;
+                    Console.WriteLine("Write your first message: ");
+                    while (!String.IsNullOrWhiteSpace(message = Console.ReadLine()))
+                    {
+                        var body = Encoding.UTF8.GetBytes(message);
+                        
+                        channel.BasicPublish(exchange: "logs",
+                                            routingKey: "",
+                                            basicProperties: null,
+                                            body: body);
+
+                        Console.WriteLine(" [x] Sent {0}, Enter a new one or enter empty message to exit.", message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PublishSubscribe/PublishSubscribe.ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace HelloWorld.Consumer
+{
+    class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+                    Console.Write(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, ea) => {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Hit CTRL+C to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}

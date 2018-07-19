1759d7df79bb243676b7c866386f63ff11fa4be9
Nabil Droussi <nabil.droussi@aurea.com>
2018-07-19 19:36:24 +0000

    Implemented topic to filter on queues to bind to.

---
+++ 5. Topics/Topics.EmitLog/EmitLog.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Publisher
+{
+    class EmitLog
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" [*] Program started, enter empty line to exit.");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                    
+                    String message;
+                    Console.WriteLine("Write your first message: ");
+                    while (!String.IsNullOrWhiteSpace(message = Console.ReadLine()))
+                    {
+                        var tmp = message.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if(tmp.Length != 2)
+                        {
+                            Console.WriteLine(" [*] Invalid entry, skipping.");
+                        }
+
+                        var severity = tmp[0].Trim();
+
+                        var body = Encoding.UTF8.GetBytes(message);
+                        
+                        channel.BasicPublish(exchange: "topic_logs",
+                                            routingKey: severity,
+                                            basicProperties: null,
+                                            body: body);
+
+                        Console.WriteLine(" [x] Sent {0}, Enter a new one or enter empty message to exit.", message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 5. Topics/Topics.ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace HelloWorld.Consumer
+{
+    class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    foreach(var severity in args)
+                    {
+                        Console.WriteLine(" [*] Binding to severity {0}", severity);
+                        channel.QueueBind(queue: queueName, exchange: "topic_logs", routingKey: severity);
+                    }
+
+                    Console.Write(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, ea) => {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Hit CTRL+C to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
